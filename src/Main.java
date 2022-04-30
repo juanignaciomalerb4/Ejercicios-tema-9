742c7f1ee1d5ef48a00cd50e7341d255e062b5c3
juanignaciomalerb4 <tech.jimalerba@gmail.com>
2022-04-30 13:58:34 -0300

    ejercicios tema 9

---
+++ src/Main.java
@@ -0,0 +1,71 @@
+public class Main {
+    public static void main (String [] args) {
+
+        Cliente cliente = new Cliente();
+        cliente.setEdad(51);
+        cliente.setNombre("Ernesto");
+        cliente.setTelefono("+54 9 3434 123-4567");
+        cliente.setCredito(true);
+
+        System.out.println( cliente.getEdad());
+        System.out.println(cliente.getNombre());
+        System.out.println(cliente.getTelefono());
+        System.out.println(cliente.getCredito());
+
+
+        Trabajador trabajador = new Trabajador();
+        trabajador.setEdad(25);
+        trabajador.setNombre("Juan Ignacio");
+        trabajador.setTelefono("+54 9 3435 123-4455");
+        trabajador.setSalario(200000.00);
+    }
+}
+
+class Persona {
+    int edad;
+    String nombre;
+    String telefono;
+
+        public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    public int getEdad() {
+        return this.edad;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getNombre() {
+        return this.nombre = nombre;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+    public String getTelefono() {
+        return this.telefono;
+    }
+}
+
+class Cliente extends Persona {
+    boolean credito;
+
+    public void setCredito(boolean credito) {
+        this.credito = credito;
+    }
+    public boolean getCredito(){
+        return this.credito;
+    }
+}
+
+class Trabajador extends Persona {
+    double salario;
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+    public double getSalario() {
+        return this.salario;
+    }
+}
